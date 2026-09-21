7b355fb2fd6deb57aef88946a0c5fd9edfb4cec5
Tyler DeVries <tylerdevries22@gmail.com>
2026-09-21 00:44:28 -0600

    test(e2e): retain safe actionable remote failure locations

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   fullyParallel: false,
   workers: 1,
-  reporter: [['list']],
+  reporter: [['list'], ['./scripts/e2e-failure-reporter.ts']],
   use: {
     baseURL: process.env.E2E_BASE_URL || 'http://127.0.0.1:3005',
     trace: 'retain-on-failure'
